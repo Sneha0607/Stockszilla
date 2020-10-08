b2b7a3c22f0208f95037f6409b7681eabf9c9608
Sneha Singh <sneha.rose0607@gmail.com>
2020-10-08 21:57:50 +0530

    Initial Commit

---
+++ Stockszilla/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from .import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homepage),
+    path('accounts/', include('accounts.urls')),
+]
+
+urlpatterns += staticfiles_urlpatterns()
